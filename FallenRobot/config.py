2083ae86fedfa258004407c501a2d29691f802a3
YASHU <130467694+nillcoderbot@users.noreply.github.com>
2023-05-05 23:26:53 +0530

    config.py

--- FallenRobot/config.py
+++ FallenRobot/config.py
@@ -5,32 +5,32 @@ class Config(object):
     API_ID = "17273188"
     API_HASH = "a2e5bb2b69d13ba7553941af16cc2d5b"
 
-    CASH_API_KEY = ""  # Get this value for currency converter from https://www.alphavantage.co/support/#api-key
+    CASH_API_KEY = "5LM757IEFP878M5I"  # Get this value for currency converter from https://www.alphavantage.co/support/#api-key
 
     DATABASE_URL = ""  # A sql database url from elephantsql.com
 
     EVENT_LOGS = ()  # Event logs channel to note down important bot level events
 
-    MONGO_DB_URI = ""  # Get ths value from cloud.mongodb.com
+    MONGO_DB_URI = "mongodb+srv://yashu:yashu@cluster0.6ntqt2r.mongodb.net/?retryWrites=true&w=majority"  # Get ths value from cloud.mongodb.com
 
     # Telegraph link of the image which will be shown at start command.
     START_IMG = "https://te.legra.ph/file/a058e18153b55d10c03f5.jpg"
 
     SUPPORT_CHAT = "night_talks_m"  # Your Telegram support group chat username where your users will go and bother you
 
-    TOKEN = ""  # Get bot token from @BotFather on Telegram
+    TOKEN = "6199003936:AAFzc8p_vbIfQCe0AeVZ2saQJh5kaSH4PNM"  # Get bot token from @BotFather on Telegram
 
-    TIME_API_KEY = ""  # Get this value from https://timezonedb.com/api
+    TIME_API_KEY = "BPKZO6L9XVL8"  # Get this value from https://timezonedb.com/api
 
     OWNER_ID = 5538947006  # User id of your telegram account (Must be integer)
 
     # Optional fields
-    BL_CHATS = []  # List of groups that you want blacklisted.
-    DRAGONS = []  # User id of sudo users
-    DEV_USERS = []  # User id of dev users
-    DEMONS = []  # User id of support users
-    TIGERS = []  # User id of tiger users
-    WOLVES = []  # User id of whitelist users
+    BL_CHATS = [5538947006]  # List of groups that you want blacklisted.
+    DRAGONS = [5538947006]  # User id of sudo users
+    DEV_USERS = [5538947006]  # User id of dev users
+    DEMONS = [5538947006]  # User id of support users
+    TIGERS = [5538947006]  # User id of tiger users
+    WOLVES = [5538947006]  # User id of whitelist users
 
     ALLOW_CHATS = True
     ALLOW_EXCL = True
